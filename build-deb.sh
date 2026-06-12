340c04a131ace3c32caabfafa93064106ff4e680
Ramyar <ramyarburhan26@gmail.com>
2026-06-12 15:30:47 +0100

    add desktop entry and icon to deb package

--- build-deb.sh
+++ build-deb.sh
@@ -15,6 +15,8 @@ echo "Creating package structure..."
 mkdir -p "$PKG/DEBIAN"
 mkdir -p "$PKG/usr/local/bin"
 mkdir -p "$PKG/usr/share/polkit-1/actions"
+mkdir -p "$PKG/usr/share/applications"
+mkdir -p "$PKG/usr/share/icons/hicolor/256x256/apps"
 
 cat > "$PKG/DEBIAN/control" <<EOF
 Package: burnerrx
@@ -28,10 +30,23 @@ EOF
 cp build/bin/burnerRx "$PKG/usr/local/bin/burnerRx"
 cp helper/burnerrx-helper "$PKG/usr/local/bin/burnerrx-helper"
 cp org.burnerrx.policy "$PKG/usr/share/polkit-1/actions/org.burnerrx.policy"
+cp build/appicon.png "$PKG/usr/share/icons/hicolor/256x256/apps/burnerrx.png"
+
+cat > "$PKG/usr/share/applications/burnerrx.desktop" <<EOF
+[Desktop Entry]
+Name=BurnerRx
+Comment=Flash ISO images to USB drives
+Exec=/usr/local/bin/burnerRx
+Icon=burnerrx
+Type=Application
+Categories=Utility;
+EOF
 
 chmod 755 "$PKG/usr/local/bin/burnerRx"
 chmod 755 "$PKG/usr/local/bin/burnerrx-helper"
 chmod 644 "$PKG/usr/share/polkit-1/actions/org.burnerrx.policy"
+chmod 644 "$PKG/usr/share/icons/hicolor/256x256/apps/burnerrx.png"
+chmod 644 "$PKG/usr/share/applications/burnerrx.desktop"
 
 echo "Building .deb..."
 dpkg-deb --build "$PKG"
